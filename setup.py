51dbfce8fc6d6ac3c4f83097d2f642a7a2134bcf
vibea <vincent.beauvivre@smile.fr>
2020-10-05 09:52:42 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NLP_API-pkg-vibby", # Replace with your own username
+    version="0.0.1",
+    author="Vibby",
+    author_email="contact@vibby.fr",
+    description="Expose some NLTK tools to HTTP API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vibby/NLP_API",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
